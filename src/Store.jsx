ef506e57a32f7e09e8d266971736653958339302
Avinash sharma <sharmaavi2196@gmail.com>
2025-03-22 21:33:59 +0530

    refactor: migrate components to JSX and implement Redux store for account management

--- src/Store.jsx
+++ src/Store.jsx
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -20,7 +22,9 @@ const reducer = (state = initialState, action) => {
       if (state.loan > 0) return state;
       return {
         ...state,
-        loan: action.payload,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
       };
     case "account/payloan":
       return {
@@ -33,3 +37,12 @@ const reducer = (state = initialState, action) => {
       return state;
   }
 };
+
+const store = createStore(reducer);
+store.dispatch({ type: "account/deposit", payload: 1000 });
+store.dispatch({
+  type: "account/requisition",
+  payload: { amount: 1000, purpose: "paying bills" },
+});
+
+console.log(store.getState());
