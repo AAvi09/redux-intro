08ef6167391638053550e59242110e5e6bb40b9f
Avinash sharma <sharmaavi2196@gmail.com>
2025-03-24 01:23:11 +0530

    feat: add react-redux for state management and connect Customer component to Redux store

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import store from "./Store.jsx";
+import { Provider } from "react-redux";
 
 store.dispatch({ type: "account/deposit", payload: 1000 });
 console.log(store.getState());
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
