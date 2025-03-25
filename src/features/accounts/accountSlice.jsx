08c56195154cbe62c815663d7efb50782ba0e47d
Avinash sharma <sharmaavi2196@gmail.com>
2025-03-26 02:02:54 +0530

    feat: enhance account operations with currency conversion and loading state management

--- src/features/accounts/accountSlice.jsx
+++ src/features/accounts/accountSlice.jsx
@@ -2,6 +2,7 @@ const initialStateAccount = {
   balance: 0,
   loan: 0,
   loanPurpose: "",
+  isLoading: false,
 };
 
 const accountReducer = (state = initialStateAccount, action) => {
@@ -10,6 +11,7 @@ const accountReducer = (state = initialStateAccount, action) => {
       return {
         ...state,
         balance: state.balance + action.payload,
+        isLoading: false,
       };
     case "account/withdraw":
       return {
@@ -31,6 +33,12 @@ const accountReducer = (state = initialStateAccount, action) => {
         loanPurpose: "",
         balance: state.balance - state.loan,
       };
+
+    case "account/convertingCurrency":
+      return {
+        ...state,
+        isLoading: true,
+      };
     default:
       return state;
   }
@@ -43,8 +51,20 @@ const accountReducer = (state = initialStateAccount, action) => {
 
 // console.log(store.getState());
 
-export function deposit(amount) {
-  return { type: "account/deposit", payload: amount };
+export function deposit(amount, currency) {
+  if (currency === "USD") return { type: "account/deposit", payload: amount };
+
+  return async function (dispatch, getState) {
+    dispatch({ type: "account/convertingCurrency" });
+    const res = await fetch(
+      `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+    );
+    const data = await res.json();
+    // console.log(data);
+    const converted = data.rates.USD;
+
+    dispatch({ type: "account/deposit", payload: converted });
+  };
 }
 export function withdraw(amount) {
   return { type: "account/withdraw", payload: amount };
